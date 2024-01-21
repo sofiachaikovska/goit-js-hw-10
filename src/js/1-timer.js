3dbe8ec3ca45d1386e3b21fe938e4c769c2b78c4
Sofia Chaikovs'ka <sofiya0511@gmail.com>
2024-01-21 14:49:09 +0200

    Added libraries

---
+++ src/js/1-timer.js
@@ -0,0 +1,22 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+  flatpickr('#datetime-picker', {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  });
+});
+
+iziToast.show({
+  title: 'Error',
+  message: 'Please choose a date in the future',
+});
